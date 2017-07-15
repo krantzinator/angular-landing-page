f605b6d4aaf7dcc8f5e2f14a4d802f54417c8ebd
krantzinator <krantzrs8@gmail.com>
2017-07-15 17:18:01 -0400

    made material work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MdCardModule, MdButtonModule, MdInputModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
@@ -10,6 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     MdCardModule,
     MdButtonModule,
     MdInputModule
